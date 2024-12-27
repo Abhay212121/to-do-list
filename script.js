ef165a9d90bf7b9c20cb1a1dabcc7f9a323da914
Abhay <abhay.work.02@gmail.com>
2024-12-27 22:28:26 +0530

    added functionality to the 'more-options' button in the lower sidebar.

--- script.js
+++ script.js
@@ -4,20 +4,58 @@ let bannerHead = document.querySelector('.banner-head')
 let newProjectBtn = document.querySelector('#project-submit-btn')
 let allItems = document.querySelector('.all-items')
 let addBtn, cancelBtn;
-
+let dropBoxBtns = document.querySelectorAll('.more-options')
+let dropBox = document.querySelector('.dropbox')
+let lowerSideBarListItems = document.querySelectorAll('.lower-item')
+let dropboxDiv;
 
 let newProjectBtnCount = 1;
 newProjectBtn.addEventListener('click', () => {
-    if (newProjectBtnCount == 1) {
-        addNewProjectBox()
-        newProjectBtnCount = 0
+  if (newProjectBtnCount == 1) {
+    addNewProjectBox()
+    newProjectBtnCount = 0
+  }
+})
+
+document.addEventListener('click', (e) => {
+  if (dropboxDiv != undefined) {
+    dropboxDiv.remove()
+  }
+})
+
+
+lowerSideBarListItems.forEach((listItem) => {
+
+  listItem.lastElementChild.addEventListener('click', (e) => {
+    if (dropboxDiv == undefined) {
+      dropboxDiv = document.createElement('div')
+      dropboxDiv.classList.add('dropbox');
+      dropboxDiv.innerHTML =
+        `<div class="edit-btn">Rename</div>
+      <div class="delete-btn">Delete</div>`
+      listItem.prepend(dropboxDiv)
+      e.stopPropagation()
     }
+
+    else {
+      dropboxDiv.remove()
+      dropboxDiv = document.createElement('div')
+      dropboxDiv.classList.add('dropbox');
+      dropboxDiv.innerHTML =
+        `<div class="edit-btn">Rename</div>
+      <div class="delete-btn">Delete</div>`
+      listItem.prepend(dropboxDiv)
+      e.stopPropagation()
+
+    }
+
+  })
 })
 
 function addNewProjectBox() {
-    let div = document.createElement('div');
-    div.innerHTML =
-        `<div class="new-project-inputbox-section">
+  let div = document.createElement('div');
+  div.innerHTML =
+    `<div class="new-project-inputbox-section">
     <div class="new-project-inputbox-section-above">
       <img
         src="images/burger-bar.png"
@@ -35,31 +73,29 @@ function addNewProjectBox() {
       <button class="btn2">Cancel</button>
     </div>
   </div>`
-    allItems.append(div);
-    addBtn = document.querySelector('.btn1');
-    cancelBtn = document.querySelector('.btn2')
-    handlingTheNewBtns(div)
+  allItems.append(div);
+  addBtn = document.querySelector('.btn1');
+  cancelBtn = document.querySelector('.btn2')
+  handlingTheNewBtns(div)
 }
 
 function handlingTheNewBtns(div) {
-    addBtn.addEventListener('click', () => {
-        newProjectBtnCount = 1
-        let val = document.querySelector('#new-project-inputbox').value
-        addingNewDynamicItemInList(val)
-        div.remove()
-    })
-    cancelBtn.addEventListener('click', () => {
-        newProjectBtnCount = 1
-        div.remove()
-    })
+  addBtn.addEventListener('click', () => {
+    newProjectBtnCount = 1
+    let val = document.querySelector('#new-project-inputbox').value
+    addingNewDynamicItemInList(val)
+    div.remove()
+  })
+  cancelBtn.addEventListener('click', () => {
+    newProjectBtnCount = 1
+    div.remove()
+  })
 }
 
-
-
 function addingNewDynamicItemInList(val) {
-    let div = document.createElement('div');
-    div.innerHTML =
-        `<div class="item">
+  let div = document.createElement('div');
+  div.innerHTML =
+    `<div class="item">
     <img
       src="images/burger-bar.png"
       alt="icon not found"
@@ -73,13 +109,16 @@ function addingNewDynamicItemInList(val) {
     />
   </div>`
 
-    allItems.append(div)
+  allItems.append(div)
 }
 
-listItems.forEach((item) => {
-    item.addEventListener('click', () => {
-        bannerHead.textContent = item.textContent
-    })
-})
+// listItems.forEach((item) => {
+//   item.addEventListener('click', (e) => {
+//     e.stopPropagation()
+//     console.log(item)
+//     // bannerHead.textContent = item[2].textContent
+//   })
+
+// })
 
 
